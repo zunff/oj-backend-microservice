3c410b5217e78797f6b105229ddad57bf7d4de5d
zunf <1430119384@qq.com>
2025-07-11 16:28:19 +0800

    [update] spring ai 初始化

---
+++ oj-backend-ai-service/src/main/java/cn/com/zunf/ojbackendaiservice/OjBackendAiServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.com.zunf.ojbackendaiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjBackendAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjBackendAiServiceApplication.class, args);
+	}
+
+}
---
+++ oj-backend-ai-service/src/test/java/cn/com/zunf/ojbackendaiservice/OjBackendAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.zunf.ojbackendaiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjBackendAiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
